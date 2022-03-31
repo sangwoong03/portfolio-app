9225f9c862a841b46229ae88fa047a0e30c4141e
sangwoong <sangwoong03@gmail.com>
2022-03-31 20:11:51 +0900

    portfolio_education

--- src/components/Education.js
+++ src/components/Education.js
@@ -4,25 +4,25 @@ import "./Education.css";
 function Education() {
 	const eduInfo = [
 		{
-			institute: "SEMA HIGHSCHOLL",
+			institute: "SEMA Highschool",
 			major: "-",
 			terms: "2012. 03 ~ 2015. 02",
 			grade: "Graduated",
 		},
 		{
-			institute: "AJOU UNIVERSITY",
+			institute: "AJOU University",
 			major: "English Lang. and Lit.",
 			terms: "2015. 03 ~ 2019. 02",
 			grade: "3.7 / 4.5 ",
 		},
 		{
-			institute: "ZEROBASE",
+			institute: "Zerobase",
 			major: "Front-end Programming",
 			terms: "2021. 11 ~ 2022. 01",
 			grade: "Passed",
 		},
 		{
-			institute: "WECODE BOOTCAMP",
+			institute: "Wecode Bootcamp",
 			major: "Front-end Programming",
 			terms: "2022. 05 ~ ",
 			grade: "not yet",
