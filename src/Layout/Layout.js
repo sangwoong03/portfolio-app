6bb9cb0547b68d25e7408cfc0376b772175f7ef4
sangwoong <sangwoong03@gmail.com>
2022-04-01 10:54:59 +0900

    portfolio_layout

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React, { Fragment } from "react";
-import About from "../components/About";
 // 컴포넌트 컴파일 시 불필요한 element(div) 확산 방지
 
 import Header from "../components/Header";
 import MainSection from "../components/MainSection";
-import Projects from "../components/Project/Project";
 import SideNav from "../components/SideNav/SideNav";
+import About from "../components/About";
+import Projects from "../components/Project/Project";
+import Contact from "../components/Contact/Contact";
 
 function Layout() {
 	return (
@@ -15,6 +16,7 @@ function Layout() {
 			<MainSection />
 			<About />
 			<Projects />
+			<Contact />
 		</Fragment>
 	);
 }
