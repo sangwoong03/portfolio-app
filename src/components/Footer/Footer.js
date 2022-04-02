d0825eada74e9077e3bee1ce284ba03ede8bd98c
sangwoong <sangwoong03@gmail.com>
2022-04-02 11:07:36 +0900

    portfolio_footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Footer.css";
+import { Container } from "reactstrap";
+
+function Footer() {
+	return (
+		<footer>
+			<Container className="footer__wrapper">
+				<p> This shall too pass away </p>
+			</Container>
+		</footer>
+	);
+}
+
+export default Footer;
