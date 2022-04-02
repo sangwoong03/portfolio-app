13501d5dd2b40c8f3d0ff70daa21cbcb5d32e7f9
sangwoong <sangwoong03@gmail.com>
2022-04-02 11:07:32 +0900

    portfolio_contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -12,13 +12,13 @@ function Contact() {
 						<h2> Contact ME </h2>
 					</Col>
 
-					<Col lg="6" md="6">
-						<div className="contact__info-container d-flex gap-5">
-							<div className="contact__info-box w-50">
+					<Col lg="6" md="6" className="contact__info">
+						<div className="contact__info-container">
+							<div className="contact__info-box ">
 								<h6> Phone</h6>
 								<p> +82-10-3770-7063</p>
 							</div>
-							<div className="contact__info-box w-50">
+							<div className="contact__info-box ">
 								<h6> GitHub </h6>
 								<p>
 									<a href="https://github.com/sangwoong03">@sangwoong03 </a>
@@ -26,18 +26,18 @@ function Contact() {
 							</div>
 						</div>
 
-						<div className="contact__info-container d-flex gap-5">
-							<div className="contact__info-box w-50">
+						<div className="contact__info-container">
+							<div className="contact__info-box ">
 								<h6> Email</h6>
 								<p> sangwoong03@gmail.com </p>
 							</div>
-							<div className="contact__info-box w-50">
+							<div className="contact__info-box ">
 								<h6> Blog </h6>
 								<p> setting... </p>
 							</div>
 						</div>
 					</Col>
-					<Col>
+					<Col lg="6" md="6" className="contact__form">
 						<Form />
 					</Col>
 				</Row>
