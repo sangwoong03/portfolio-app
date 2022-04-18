3676312248d83405d91f73d67d969978839a77ca
sangwoong <sangwoong03@gmail.com>
2022-04-18 17:28:53 +0900

    docs: portfolio_layout (edit the route of modules)

--- src/Layout/Layout.js
+++ src/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React, { Fragment } from "react";
 // 컴포넌트 컴파일 시 불필요한 element(div) 확산 방지
 
-import Header from "../components/Header";
-import MainSection from "../components/MainSection";
+import Header from "../components/Header/Header";
+import MainSection from "../components/Main/MainSection";
 import SideNav from "../components/SideNav/SideNav";
-import About from "../components/About";
+import About from "../components/About/About";
 import Projects from "../components/Project/Project";
 import Contact from "../components/Contact/Contact";
+import Footer from "../components/Footer/Footer";
 
 function Layout() {
 	return (
@@ -17,6 +18,7 @@ function Layout() {
 			<About />
 			<Projects />
 			<Contact />
+			<Footer />
 		</Fragment>
 	);
 }
